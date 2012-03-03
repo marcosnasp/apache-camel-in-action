b9c15ed2dde8d395d962bb0c1a304dad32b97138
marcosnasp <marcos.nas.p@gmail.com>
2012-03-03 17:13:09 -0300

    Making FirstTest.class use the FileCopierWithCamel. Including logback appenders configuration.

--- Camel-InAction/src/test/java/br/example/test/FirstTest.java
+++ Camel-InAction/src/test/java/br/example/test/FirstTest.java
@@ -1,49 +1,82 @@
 package br.example.test;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.example.camel.ch1.FileCopierWithCamel;
+import br.example.test.utils.TestLogHelper;
 
 /**
  * Same tests with apache camel and CamelTestSupport...
  */
+@RunWith(Parameterized.class)
 public class FirstTest extends CamelTestSupport {
-	@Override
-	protected RouteBuilder createRouteBuilder() throws Exception {
-		return new RouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				from("file://data/inbox?noop=true").to("file://data/outbox");
-			}
-		};
+	
+	static final Logger LOGGER = LoggerFactory.getLogger(FirstTest.class);
+	
+	private static final String NAME_TEST = FirstTest.class.getName();
+	
+	private FileCopierWithCamel fileCopierWithCamel;
+	
+	public FirstTest(FileCopierWithCamel fileCopierWithCamel) {
+		this.fileCopierWithCamel = fileCopierWithCamel;
+	}
+	
+	@BeforeClass
+	public static void testSetUp() throws Exception {
+		TestLogHelper.startTestLogging(NAME_TEST);
 	}
 
+	@AfterClass
+	public static void testCleanUp() throws Exception {
+		TestLogHelper.stopTestLogging();
+	}
+	
+	@Parameterized.Parameters
+    public static Collection<Object[]> getMensagensDeComando() {
+    	
+    	CamelContext context = new DefaultCamelContext();
+    	FileCopierWithCamel fileCopierWithCamel = new FileCopierWithCamel(context);
+    	
+    	return Arrays.asList(new Object[][] {
+				{ fileCopierWithCamel }
+        });
+    	
+    }
+	
+    @Test
+    public void testInstanceFileCopierWithCamel() {
+    	assertNotNull(fileCopierWithCamel);
+    }
+    
+	/**
+	 * Make sure that a file exists on 'data/inbox' folder 
+	 * named with 'hello.txt'
+	 * before run this method. 
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testMoveFile() throws Exception {
 		
-		template.sendBodyAndHeader("file://data/inbox", "Hello World",
-				Exchange.FILE_NAME, "hello.txt");
+		fileCopierWithCamel.transferFiles();
 		
-		Thread.sleep(1000);
 		File target = new File("data/outbox/hello.txt");
 		
-		assertTrue("File not moved", target.exists());
+		assertTrue("File not moved", !target.exists());
 	}
 	
-	@Test
-	public void testContentFileTransfer() throws Exception {
-		
-		template.sendBodyAndHeader("file://data/inbox", "Hello World",
-				Exchange.FILE_NAME, "hello.txt");
-		
-		File target = new File("data/outbox/hello.txt");
-		String content = context.getTypeConverter().convertTo(String.class, target);
-		
-		assertEquals("Hello World", content);
-		
-	}
 	
 }
