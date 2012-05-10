7d345ebca676db3ca38a4246adf97d1d6f935a85
marcosnasp <marcos.nas.p@gmail.com>
2012-05-10 12:02:31 -0300

    Alterando a versão do camel para a versão 2.9.2. e configurando o Spring xml.

--- Camel-InAction/src/main/java/br/example/camel/ch1/JMSCamelExample.java
+++ Camel-InAction/src/main/java/br/example/camel/ch1/JMSCamelExample.java
@@ -1,25 +1,25 @@
 package br.example.camel.ch1;
 
-import javax.jms.ConnectionFactory;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import br.example.camel.ch1.routers.JMSRouterBuilder;
 
 public class JMSCamelExample {
 
-	private static final String URL_BROKER = "tcp://localhost:8161/";
-	
-	private ConnectionFactory factory;
+	private ApplicationContext ctx;
 	private CamelContext context;
 	
 	public JMSCamelExample() {
-		factory = new ActiveMQConnectionFactory(URL_BROKER);
+	    
+	    ctx = new ClassPathXmlApplicationContext("META-INF/context.xml");
+	    JmsComponent jmsComponent = ctx.getBean("jms", JmsComponent.class);
+	    
 		context = new DefaultCamelContext();
-		context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(factory));
+		context.addComponent("jms", jmsComponent);
 	}
 	
 	@SuppressWarnings("unused")
